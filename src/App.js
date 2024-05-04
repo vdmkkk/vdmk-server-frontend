dab07bee226043c06ccd55d2deb465ce25604468
vdmkkk <soloviov.vad@gmail.com>
2024-05-05 02:28:05 +0300

    added all-hosts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  async function getAllHosts () {
+    await axios.get('95.84.137.217:3001/all-hosts').then((res) => {
+      setHosts(res.data);
+    })
+  }
+
+  const [hosts, setHosts] = useState('')
+
   return (
     <div className="App">
       я люблю аришку
+      {hosts}
+      <button onClick={() => getAllHosts()}>Обновить</button>
     </div>
   );
 }
